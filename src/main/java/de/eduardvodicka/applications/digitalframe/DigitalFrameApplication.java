f71886013b3eb7c1a9d9a514f5bcb3097c40557c
Eduard Vodicka <eduard.vodicka@eforce21.com>
2015-08-13 17:20:20 +0200

    Created angular-project

---
+++ src/main/java/de/eduardvodicka/applications/digitalframe/DigitalFrameApplication.java
@@ -0,0 +1,12 @@
+package de.eduardvodicka.applications.digitalframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalFrameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalFrameApplication.class, args);
+    }
+}
---
+++ src/test/java/de/eduardvodicka/applications/digitalframe/DigitalFrameApplicationTests.java
@@ -0,0 +1,18 @@
+package de.eduardvodicka.applications.digitalframe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DigitalFrameApplication.class)
+@WebAppConfiguration
+public class DigitalFrameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
