14e24cd1e67c2a45ed1a82a330115417c80b3c23
Eduard Vodicka <eduard.vodicka@eforce21.com>
2015-12-28 12:40:59 +0100

    Refactoring

--- src/main/java/de/eduardvodicka/applications/digitalframe/DigitalFrameApplication.java
+++ src/main/java/de/eduardvodicka/applications/digitalframe/DigitalFrameApplication.java
@@ -3,12 +3,6 @@ package de.eduardvodicka.applications.digitalframe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 @SpringBootApplication
 @EnableAutoConfiguration
--- src/main/java/de/eduardvodicka/applications/digitalframe/controller/NotificationController.java
+++ src/main/java/de/eduardvodicka/applications/digitalframe/controller/NotificationController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Random;
-
 /**
  * Created by evodicka on 14.11.2015.
  */
